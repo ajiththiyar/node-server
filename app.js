0a05bbffcd02c9e27fdf0179dea9f017b48520d5
ajith thiyar <ajiththiyar@gmail.com>
2021-08-22 20:09:31 +0530

    Added Google Authentication partially

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const path = require('path')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
 const connectDB = require('./config/db')
+const session = require('express-session')
 
 // Load config
 dotenv.config({path:"./config/config.env"});
 
+// Passport config
+require('./config/passport')(passport)
 // Connect DB
 //connectDB()
 
@@ -23,10 +27,23 @@ const PORT = process.env.PORT || 5000;
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname:'.hbs'}));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 // Static folder
 app.use(express.static(path.join(__dirname,'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} Listening on PORT: ${PORT}`));
